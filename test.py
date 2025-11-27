afc285c5042a074bca6ee509860fc4e033c5758c
pragunkakkar684 <kakkarpragun.work@gmail.com>
2025-11-27 06:14:07 +0530

    bugs

--- test.py
+++ test.py
@@ -11,4 +11,8 @@ print("Added one")
 + api_key = "123-secret"
 + print("yo")
 + eval("danger")
+print("debug")
+eval("print('dangerous')")
+api_key = "sk_test_1234567890_super_secret_key_exposed_on_github_please_remove!!!!"
+very_long_line = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
