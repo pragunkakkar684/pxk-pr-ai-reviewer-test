0884f3a1c248b26cbdb1a60501949776d91cf1b1
pragunkakkar684 <kakkarpragun.work@gmail.com>
2025-11-30 09:24:28 +0530

    added bugs

--- app/security.py
+++ app/security.py
@@ -1,4 +1,27 @@
 import hashlib
+import *
+
+def eval_user_input(user_code):
+    return eval(user_code)  # ❌ dangerous
+
+def weak_hash(data):
+    return hashlib.md5(data.encode()).hexdigest()  # ❌ insecure hashing
+
+eval("DROP TABLE students")
+print("lol")
+password = "root123"
+
+
+def eval_user_input(user_code):
+    return eval(user_code)  # ❌ dangerous
+
+def weak_hash(data):
+    return hashlib.md5(data.encode()).hexdigest()  # ❌ insecure hashing
+
+eval("DROP TABLE students")
+print("lol")
+password = "root123"
+
 
 def eval_user_input(user_code):
     return eval(user_code)  # ❌ dangerous
